484446a02b07ad5e5e311f6696ca822ed755887f
blum <yuuybonito@gmail.com>
2024-07-02 14:20:44 -0300

    array list implementation

---
+++ src/include/list.h
@@ -0,0 +1,24 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+
+#include "include/types.h"
+
+void *list_create(usize type);
+void *__list_grow(void *list);
+void  list_shrink(void *list);
+void  list_shift_left(void *list, u32 index);
+void  list_shift_right(void *list, u32 index);
+void  list_destroy(void *list);
+
+#define list_capacity(LIST) (*(((usize *)(LIST)) - 3))
+#define list_size(LIST) (*(((usize *)(LIST)) - 2))
+#define list_grow(LIST) do { (LIST) = __list_grow(LIST); } while(0)
+#define list_push(LIST, ITEM) do { (LIST) = __list_grow(LIST); (LIST)[list_size(LIST) - 1] = ITEM; } while(0)
+#define list_pop(LIST) do { list_shrink(LIST); } while(0)
+#define list_insert(LIST, INDEX, ITEM) do { (LIST) = __list_grow(LIST); list_shift_right(LIST, INDEX); (LIST)[INDEX] = ITEM; } while(0)
+#define list_remove(LIST, INDEX) do { list_shift_left(LIST, INDEX); list_shrink(LIST); } while(0)
+#define list_push_back(LIST, ITEM) list_insert(LIST, 0, ITEM)
+#define list_pop_back(LIST) list_remove(LIST, 0)
+
+#endif/*__LIST_H__*/
--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
+#include "include/core.h"
 #include "include/types.h"
 #include "include/window.h"
 #include "include/renderer.h"
 
+#include "include/list.h"
+
 i32
 main(void) {
+  u32 *numbers = list_create(sizeof (u32));
+  list_push(numbers, 0);
+  list_push(numbers, 1);
+  list_push(numbers, 2);
+  list_push(numbers, 3);
+  list_push(numbers, 4);
+  list_push(numbers, 5);
+  list_push(numbers, 6);
+  list_push(numbers, 7);
+  for (u32 i = 0; i < list_size(numbers); i++) { INFO("numbers[%u] = %u", i, numbers[i]); } INFO();
+  list_pop(numbers);
+  list_pop(numbers);
+  for (u32 i = 0; i < list_size(numbers); i++) { INFO("numbers[%u] = %u", i, numbers[i]); } INFO();
+  list_insert(numbers, 2, 8);
+  list_insert(numbers, 2, 9);
+  for (u32 i = 0; i < list_size(numbers); i++) { INFO("numbers[%u] = %u", i, numbers[i]); } INFO();
+  list_remove(numbers, 5);
+  list_remove(numbers, 0);
+  for (u32 i = 0; i < list_size(numbers); i++) { INFO("numbers[%u] = %u", i, numbers[i]); } INFO();
+  list_destroy(numbers);
+
+  return 0;
   window_create();
   renderer_create();
   while (window_is_running()) {
